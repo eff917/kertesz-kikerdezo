b81b3f0906fe19435edcd1900cbcd56537babb73
Eff <effpeter@gmail.com>
2021-11-27 00:27:46 +0100

    added list detail page skeleton

---
+++ frontend/src/routes/lists/[id].svelte
@@ -0,0 +1,23 @@
+<script context='module'>
+    import { fetchListByID } from '../../stores/lists';
+    import { plantList } from '../../stores/plants';
+
+    let listsValue;
+    export async function load(ctx) {
+        let id = ctx.page.params.id;
+
+        const selectedList = await fetchListByID(id)
+        return { props: { selectedList }}
+    }
+</script>
+
+<script>
+    export let selectedList;
+</script>
+
+<div class="flex flex-col items-center">
+    <h1 class="text-4xl text-center my-8 uppercase">{selectedList.name}</h1>
+    {#each selectedList.plants as plant}
+    <p class="text">{plant.latin_name}</p>
+    {/each}
+</div>
